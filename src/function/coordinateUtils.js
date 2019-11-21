03961d0e3a512d5515167f85a8b4b113a936461f
陈亮亮 <cll1@strongsoft.net>
2019-11-21 21:09:22 +0800

    update

--- src/function/coordinateUtils.js
+++ src/function/coordinateUtils.js
@@ -207,6 +207,89 @@ const wgs84ToBd09 = (lng, lat) => {
 
 }
 
+/**
+ * WGS84 坐标转 Cgcs2000 坐标
+ */
+const wgs84toCgcs2000 = (lat, lng) => {
+
+  let pi = 3.14159265358979324
+  let a = 6378245.0
+  let ee = 0.00669342162296594323
+
+  let transformLon = function (x, y) {
+
+    let ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * Math.sqrt(Math.abs(x))
+    ret += (20.0 * Math.sin(6.0 * x * pi) + 20.0 * Math.sin(2.0 * x * pi)) * 2.0 / 3.0
+    ret += (20.0 * Math.sin(x * pi) + 40.0 * Math.sin(x / 3.0 * pi)) * 2.0 / 3.0
+    ret += (150.0 * Math.sin(x / 12.0 * pi) + 300.0 * Math.sin(x / 30.0 * pi)) * 2.0 / 3.0
+
+    return ret
+
+  }
+  
+  let transformLat = function (x, y) {
+
+    let ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * Math.sqrt(Math.abs(x))
+
+    ret += (20.0 * Math.sin(6.0 * x * pi) + 20.0 * Math.sin(2.0 * x * pi)) * 2.0 / 3.0
+    ret += (20.0 * Math.sin(y * pi) + 40.0 * Math.sin(y / 3.0 * pi)) * 2.0 / 3.0
+    ret += (160.0 * Math.sin(y / 12.0 * pi) + 320 * Math.sin(y * pi / 30.0)) * 2.0 / 3.0
+
+    return ret
+
+  }
+
+  let point = {}
+
+  let outOfChina = function(lat, lon) {
+
+    if (lon < 72.004 || lon > 137.8347) {
+
+      return true
+
+    }
+
+    if (lat < 0.8293 || lat > 55.8271) {
+
+      return true
+
+    }
+      
+    return false
+
+  }
+
+  if ( outOfChina(lat, lng) ) {
+
+    point.lat = lat
+    point.lng = lng
+
+    return point
+
+  }
+
+  let dLat = transformLat(lng - 105.0, lat - 35.0)
+  let dLon = transformLon(lng - 105.0, lat - 35.0)
+  let radLat = lat / 180.0 * pi
+  let magic = Math.sin(radLat)
+
+  magic = 1 - ee * magic * magic
+
+  let sqrtMagic = Math.sqrt(magic)
+  
+  dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * pi)
+  dLon = (dLon * 180.0) / (a / sqrtMagic * Math.cos(radLat) * pi)
+
+  let lat = lat + dLat
+  let lon = lng + dLon
+
+  point.lat=lat
+  point.lng=lon
+
+  return point
+
+}
+
 export default {
 
   bd09ToGcj02,
@@ -214,6 +297,7 @@ export default {
   gcj02ToWGS84,
   wgs84ToGcj02,
   bd09ToWGS84,
-  wgs84ToBd09
+  wgs84ToBd09,
+  wgs84toCgcs2000
   
 }
